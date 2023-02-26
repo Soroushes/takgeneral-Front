706de577db479003ce18e6c1b05252ee2244eb6d
soroushes1 <esmailian.soroush246@gmail.com>
2023-02-26 23:06:07 +0330

    init project

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
-import '@/styles/globals.css'
-
+import '../styles/styles.css' ;
+import theme from "@/styles/theme/theme";
+import {ThemeProvider} from "@mui/material";
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={theme}><Component {...pageProps} /></ThemeProvider>
 }
---
+++ src/styles/theme/theme.js
@@ -0,0 +1,13 @@
+import {createTheme} from "@mui/material";
+
+const theme = createTheme({
+    palette : {
+        primary : {
+            main : '#999'
+        }
+    } ,
+    typography : {
+        fontFamily : "takgeneral "
+    }
+}) ;
+export default theme ;
