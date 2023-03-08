1377b793c4ec4c0e63744d247d18cf464b7533fe
soroushes1 <esmailian.soroush246@gmail.com>
2023-03-09 02:48:07 +0330

    shirbarghi gaz

--- src/data/header.js
+++ src/data/header.js
@@ -13,7 +13,7 @@ export const headerItem = [
         icon: <ExactToolIcon/>,
     },
     {
-        name: "شیر برقی کاز",
+        name: "شیر برقی گاز",
 
         icon: <ShirbarghiIcon/>,
     },
