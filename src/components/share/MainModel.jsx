78e60a6479a3a01dfefb10d4675c894a21fe7461
hastifadaee <hasti.fadaee2021@gmail.com>
2023-03-26 09:45:21 -0700

    Main model

---
+++ src/components/share/MainModel.jsx
@@ -0,0 +1,35 @@
+import { Box, Drawer, Typography } from "@mui/material";
+const MainModel = ({drawerIsOpen ,setDrawerIsOpen}) => {   
+
+    return (
+    <Drawer
+    sx={{width :'100%' , display : 'flex' , flexDirection : 'column'}}
+      anchor={"bottom"}
+      open={drawerIsOpen}
+      onClose={() => setDrawerIsOpen(false)}
+    >
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+        <h1>sdfghjl;'asdfghjl;'asdfghjlertopsxcvbnm,dfghj</h1>
+            </Drawer>
+  );
+};
+export default MainModel;
--- src/pages/category/[category]/[categoryType].jsx
+++ src/pages/category/[category]/[categoryType].jsx
@@ -1,10 +1,23 @@
 import {Box} from "@mui/system";
 import Image from "next/image";
-import {Grid, Container} from "@mui/material";
+import {Grid, Container, Typography } from "@mui/material";
 import categoryBanner from '../../../../public/banner.png'
 import ProductPreviewCard from "../../../components/share/ProductPreviewCard";
 import CheckBoxFilter from "src/components/share/CheckBoxFilter";
+import MainModel from "src/components/share/MainModel";
+import { useState } from "react";
+
 const Category = () => {
+    const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+    const toggleDrawer = (event) => {
+        if (
+            event.type === "keydown" &&
+            (event.key === "Tab" || event.key === "Shift")
+        ) {
+            return;
+        }
+        setDrawerIsOpen(prev => !prev);
+    };
     const brandFilter ={
         title :'برند های پمپ', 
         values:[
@@ -24,10 +37,16 @@ const Category = () => {
                 <Image fill alt={''} src={categoryBanner}/>
             </Box>
             <Container maxWidth={'lg'}>
-                <Grid container>
+                <Grid container rowGap={2}>
                     <Grid container xs={3} sx={{pr: 2, display: {xs: 'none', md: "block"}}}>
                         <Box sx={{height: 300, width: "100%"}}>
                             <CheckBoxFilter subFilter={brandFilter}/>
+                        </Box>
+                    </Grid>
+                    <Grid container xs={12} sx={{ width :'100%', display: {xs: 'block', md: "none"} , borderRadius:2}}>
+                        <Box sx={{width :'100%' , height:'50px' ,borderBottom :'1px solid #ccc' , display :'flex' , flexDirection :'column' , justifyContent :'center'}}>
+                            <Typography onClick={toggleDrawer}>فیلتر</Typography>
+                            <MainModel sx={{width :'100%'}} drawerIsOpen={drawerIsOpen} setDrawerIsOpen={setDrawerIsOpen}/>
                         </Box>
                     </Grid>
                     <Grid rowGap={4} xs={12} md={9} container sx={{borderRadius: 2, boxShadow: 3}}>
