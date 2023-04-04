e7a656e196815baf4e0d699e3b9f502b55163958
soroushes1 <esmailian.soroush246@gmail.com>
2023-04-04 20:05:37 +0430

    css cache

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -14,24 +14,29 @@ const cacheRtl = createCache({
     key: 'muirtl',
     stylisPlugins: [prefixer, rtlPlugin],
 });
-
+const cssCache = createCache({
+    key: 'css',
+    prepend: true
+})
 export default function App({Component, pageProps}) {
     return (
-        <CacheProvider value={cacheRtl}>
-            <ThemeProvider theme={theme}>
-                <Provider store={store}>
-                    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
-                          integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossOrigin=""/>
-                    <Head>
-                        <meta name="viewport"
-                              content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
-                        <title>فروشگاه اینترنتی تک جنرال</title>
-                    </Head>
-                    <Layout>
-                        <Component {...pageProps} />
-                    </Layout>
-                </Provider>
-            </ThemeProvider>
+        <CacheProvider value={cssCache}>
+            <CacheProvider value={cacheRtl}>
+                <ThemeProvider theme={theme}>
+                    <Provider store={store}>
+                        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+                              integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossOrigin=""/>
+                        <Head>
+                            <meta name="viewport"
+                                  content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+                            <title>فروشگاه اینترنتی تک جنرال</title>
+                        </Head>
+                        <Layout>
+                            <Component {...pageProps} />
+                        </Layout>
+                    </Provider>
+                </ThemeProvider>
+            </CacheProvider>
         </CacheProvider>
     )
 }
