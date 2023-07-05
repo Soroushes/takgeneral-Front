729e3b01992b755e9386d3ddb7cd6b1b60f45bf8
soroushes1 <esmailian.soroush246@gmail.com>
2023-07-05 23:54:29 +0330

    seccond merge hasti-dev

--- src/app/product/[productId]/productPage.jsx
+++ src/app/product/[productId]/productPage.jsx
@@ -7,30 +7,26 @@ import CommentQuestion from "../../../components/singleProduct/Comment&Question"
 import SingleProductImage from "../../../components/singleProduct/SingleProductImage";
 import SingleProductAttribute from "../../../components/singleProduct/SingleProductAttribute";
 import SingleProductSellCard from "../../../components/singleProduct/SingleProductSellCard";
-
+import SingleImage from '../../../assets/images/Rectangle 1.png';
 const ProductPage = ({data}) => {
     const attributesTableRef = useRef(null);
-    const opinionTableRef = useRef(null)
-    const [isShowAllDetails, setIsShowAllDetails] = useState(false)
-    console.log(data);
+    const opinionTableRef = useRef(null);
+    const [isShowAllDetails, setIsShowAllDetails] = useState(false);
     return (
         <Box sx={{pt: 3, height: '100%'}}>
-            <Container maxWidth={'xl'}>
+            <Container maxWidth={'lg'}>
                 <Grid container rowGap={5}>
-                    <Grid item sm={6} md={5} lg={3.3} xs={12}>
-                        {data.product.main_image ?
-                            <SingleProductImage mainImage={data.product.main_image}
-                                                otherImage={data.product.other_images}/> : null
-                        }
+                    <Grid item md={3.5} xs={12}>
+                        <SingleProductImage mainImage={SingleImage} otherImage={null}/>
                     </Grid>
-                    <Grid item sx={{px: {sm: 3}}} sm={6} md={7} lg={5.2} xs={12}>
+                    <Grid item sx={{px: {md: 3}}} md={5} xs={12}>
                         <SingleProductAttribute
                             setShowAllDetails={setIsShowAllDetails} name={data.product.name}
-                            attributes={data.product.attributes} attrRef={attributesTableRef}
-                            opinionRef={opinionTableRef}/>
+                            attributes={data.product.attributes} attrRef={attributesTableRef}/>
                     </Grid>
-                    <Grid item sm={12} lg={3.5} xs={12}>
+                    <Grid item md={3.5} xs={12}>
                         <SingleProductSellCard
+                            opinionRef={opinionTableRef}
                             available={data.product.product_available}
                             price={data.product.price}
                             finalPrice={data.product.final_price}
