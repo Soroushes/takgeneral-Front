43c37a87ac781c806c7c8ea83df1c423a05d74ed
hastifadaee <hasti.fadaee2021@gmail.com>
2023-11-09 15:34:11 +0330

    namad

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -153,16 +153,7 @@ const Footer = () => {
                                 borderRadius: 2,
                                 boxShadow: 1
                             }}>
-                                {/*<Image referrerPolicy='origin' id='rgvjwlaooeukwlaofukzoeuk'*/}
-                                {/*       style={{cursor: 'pointer', width: '100%', height: '100%'}}*/}
-                                {/*       onClick={() => {*/}
-                                {/*           window.open("https://logo.samandehi.ir/Verify.aspx?id=348468&p=xlaoaodsmcsiaodsgvkamcsi", "Popup", "toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=450, height=630, top=30")*/}
-                                {/*       }}*/}
-                                {/*       alt={'logo-samandehi'}*/}
-                                {/*       src={test}/>*/}
-                                {/*<Image width={'120px'} height={'132px'} style={{width: '100%', height: 'auto'}}*/}
-                                {/*       src={nationalImage} alt={''}/>*/}
-                                <img referrerPolicy='origin' id='rgvjwlaooeukwlaofukzoeuk' style={{cursor: 'pointer' , width:'100%' , height:'100%'}}
+                                <Image referrerPolicy='origin' width={86} height={94} id='rgvjwlaooeukwlaofukzoeuk' style={{cursor: 'pointer' , width:'100%' , height:'100%'}}
                                      onClick={()=> {
                                          window.open("https://logo.samandehi.ir/Verify.aspx?id=348468&p=xlaoaodsmcsiaodsgvkamcsi", "Popup", "toolbar=no, scrollbars=no, location=no, statusbar=no, menubar=no, resizable=0, width=450, height=630, top=30")
                                      }}
