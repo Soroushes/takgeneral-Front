be925613dbb2492028ecea320222938f9765a290
soroushes1 <esmailian.soroush246@gmail.com>
2023-12-05 20:28:22 +0330

    rem

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -16,10 +16,8 @@ export default async function RootLayout({children}) {
         <html className={myFont.variable} dir='rtl' lang="fa-IR">
         <head>
             <script dangerouslySetInnerHTML={{
-                __html:`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W5H9B34R');`
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W5H9B34R');`
             }}/>
-            <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
-                  integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossOrigin=""/>
         </head>
         <body>
         <NextAppDirEmotionCacheProvider>
