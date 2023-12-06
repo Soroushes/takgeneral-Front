a0ebe4976f861e38ad86bbf7f527cd4dcf3f5e4f
soroushes1 <esmailian.soroush246@gmail.com>
2023-12-06 20:15:03 +0330

    back css and fonts

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,16 +1,18 @@
 import ThemeRegistry from "../components/themeRegister";
+import localFont from 'next/font/local';
 import NextAppDirEmotionCacheProvider from "@/components/emotionCache";
-// const myFont = localFont({
-//     src: '../assets/font/IRANYekan.woff',
-//     display: 'swap',
-//     variable: '--iranYekan'
-// })
+import '../assets/styles/styles.css' ;
+const myFont = localFont({
+    src: '../assets/font/IRANYekan.woff',
+    display: 'swap',
+    variable: '--iranYekan'
+})
 export const metadata = {
     viewport: 'width=device-width, initial-scale=1, maximum-scale=1'
 }
 export default async function RootLayout({children}) {
     return (
-        <html dir='rtl' lang="fa-IR">
+        <html className={myFont.variable} dir='rtl' lang="fa-IR">
         <head>
             {/*<script dangerouslySetInnerHTML={{*/}
             {/*    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W5H9B34R');`*/}
