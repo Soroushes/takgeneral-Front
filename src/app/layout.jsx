a31b1fd1946e307d2935042f3cf443d548d07835
soroushes1 <esmailian.soroush246@gmail.com>
2023-12-14 14:57:35 +0330

    script returned

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,7 +2,7 @@ import ThemeRegistry from "../components/themeRegister";
 import localFont from 'next/font/local';
 import NextAppDirEmotionCacheProvider from "@/components/emotionCache";
 import Script from "next/script";
-import '../assets/styles/styles.css' ;
+import '../assets/styles/styles.css';
 
 const myFont = localFont({
     src: '../assets/font/IRANYekan.woff',
@@ -13,9 +13,9 @@ export default async function RootLayout({children}) {
     return (
         <html className={myFont.variable} dir='rtl' lang="fa-IR">
         <head>
-            {/*<Script defer={true} async={true} id={'gtm'} dangerouslySetInnerHTML={{*/}
-            {/*    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W5H9B34R');`*/}
-            {/*}}/>*/}
+            <Script defer={true} async={true} id={'gtm'} dangerouslySetInnerHTML={{
+                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W5H9B34R');`
+            }}/>
         </head>
         <body>
         <NextAppDirEmotionCacheProvider>
