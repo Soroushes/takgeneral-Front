afd465f0ebf158f92581b3825dbf7db4adc69eb7
soroushes1 <esmailian.soroush246@gmail.com>
2023-12-20 22:09:55 +0330

    new gtm position

--- src/app/(with-layout)/layout.jsx
+++ src/app/(with-layout)/layout.jsx
@@ -1,6 +1,5 @@
 import {BASE_URL} from "@/data/urls";
 import LayoutPage from "@/components/layout/LayoutPage";
-import Script from "next/script";
 
 const getCategories = async () => {
     try {
@@ -20,9 +19,6 @@ export default async function Layout({children}) {
     const categoryNames = await getCategories();
     return (
         <>
-            <Script strategy={'lazyOnload'} defer={true} async={true} id={'gtm'} dangerouslySetInnerHTML={{
-                __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W5H9B34R');`
-            }}/>
             <LayoutPage categoryNames={categoryNames}>{children}</LayoutPage>
         </>
     );
