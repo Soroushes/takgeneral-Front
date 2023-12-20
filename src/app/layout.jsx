95af695fe3c9ac3d93a60ae691f3dfb3c67d3831
soroushes1 <esmailian.soroush246@gmail.com>
2023-12-20 22:20:10 +0330

    gtm removed

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,7 +2,6 @@ import ThemeRegistry from "../components/themeRegister";
 import localFont from 'next/font/local';
 import NextAppDirEmotionCacheProvider from "@/components/emotionCache";
 import '../assets/styles/styles.css';
-import Script from "next/script";
 
 const myFont = localFont({
     src: '../assets/font/Takgeneral.woff2',
@@ -19,9 +18,9 @@ export default async function RootLayout({children}) {
             </ThemeRegistry>
         </NextAppDirEmotionCacheProvider>
         </body>
-        <Script strategy={'lazyOnload'} defer={true} async={true} id={'gtm'} dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W5H9B34R');`
-        }}/>
+        {/*<Script  defer={true} async={true} id={'gtm'} dangerouslySetInnerHTML={{*/}
+        {/*    __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W5H9B34R');`*/}
+        {/*}}/>*/}
         </html>
     )
 }
