2c15de92e06bdf7256fb1f07cdbbd117d5845cef
hastifadaee <hasti.fadaee2021@gmail.com>
2024-06-09 12:01:33 +0330

    lazy loading home page

--- src/app/(with-layout)/page.jsx
+++ src/app/(with-layout)/page.jsx
@@ -1,13 +1,12 @@
 import Slider from "../../components/home/Slider";
 import {BASE_URL, domainName} from "@/data/urls";
-import HomePageCategorySection from "../../components/home/HomePageCategorySection";
 import ProductBanners from "../../components/share/productBanners";
-import DiscountProductSlider from "@/components/home/DiscountProductSlider";
 import Blogs from "@/components/home/blogs";
 import {notFound} from "next/navigation";
 import Error from "@/app/error";
 import logApi from "@/logApi";
 import logRoutes from "@/logRoutes";
+import dynamic from "next/dynamic";
 export const metadata = {
     title : 'خانه | تک جنرال',
     description:'با تک جنرال نگران نیاز های صنعتی و تجهیزات ابزار دقیق نباشید! تامین کننده و وارد کننده انواع محصولات صنعتی',
@@ -40,6 +39,8 @@ async function getData() {
         }
     }
 }
+const HomePageCategorySection = dynamic(()=>import('@/components/home/HomePageCategorySection'))
+const DiscountProductSlider = dynamic(()=>import('@/components/home/DiscountProductSlider'))
 
 export default async function Page() {
     const data = await getData();
@@ -60,8 +61,8 @@ export default async function Page() {
                 {/*                banners={data.mid_banner}/>*/}
                 {/*<HighRateCategorySlider products={data.popular_categories}/>*/}
                 <ProductBanners sizing={{xs: 12, md: 5.8}} banners={data.end_banner}/>
-                {/*<DiscountProductSlider title={'پیشنهادات تک جنرال'} backGroundImage={'linear-gradient(to left, #1B09F9 , #27E1BC)'}*/}
-                {/*                       products={data.special_offer_products}/>*/}
+                <DiscountProductSlider title={'پیشنهادات تک جنرال'} backGroundImage={'linear-gradient(to left, #1B09F9 , #27E1BC)'}
+                                       products={data.special_offer_products}/>
                 {/*<DifferentProductScaleSlider/>*/}
                 {
                     data.new_blogs.length ? <Blogs blogs={data.new_blogs}/> : null
