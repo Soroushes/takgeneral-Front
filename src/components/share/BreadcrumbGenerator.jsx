53e067cd8638abf6510ca30ef622511a771c00c6
hastifadaee <hasti.fadaee2021@gmail.com>
2024-06-09 12:42:55 +0330

    next script

--- src/components/share/BreadcrumbGenerator.jsx
+++ src/components/share/BreadcrumbGenerator.jsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import {Typography, Box} from "@mui/material";
 import {domainName} from "@/data/urls";
 import {useMemo} from "react";
+import Script from "next/script";
 
 const BreadcrumbGenerator = ({breadcrumb, product, hasEmptyUrl = true}) => {
     const allBreadcrumbs = useMemo(() => {
@@ -36,7 +37,7 @@ const BreadcrumbGenerator = ({breadcrumb, product, hasEmptyUrl = true}) => {
     }
     return (
         <>
-            <script
+            <Script
                 type="application/ld+json"
                 dangerouslySetInnerHTML={{__html: JSON.stringify(convertBreadcrumb())}}
             />
