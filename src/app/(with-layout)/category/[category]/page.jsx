e5578e882e5fb9d86e11f52ca1e977df1e60ed2c
hastifadaee <hasti.fadaee2021@gmail.com>
2024-06-09 16:21:06 +0330

    category performance

--- src/app/(with-layout)/category/[category]/page.jsx
+++ src/app/(with-layout)/category/[category]/page.jsx
@@ -1,11 +1,10 @@
 import {BASE_URL, domainName} from "@/data/urls";
-import ChildCategoryPage from "./childCategoryPage";
-import ParentCategoryPage from './parentCategoryPage';
 import {notFound} from "next/navigation";
 import Error from "@/app/error";
 import {metadataGenerator} from "@/hooks/metadataGenerator";
 import logApi from "@/logApi";
 import logRoutes from "@/logRoutes";
+import dynamic from "next/dynamic";
 async function getData(params, searchParams) {
     let brands = searchParams.brand ?? [];
     delete searchParams.brand;
@@ -17,7 +16,7 @@ async function getData(params, searchParams) {
         parameters.append('brand[]', brand)
     })
     logApi(BASE_URL + `products/${params.category}/?` + parameters.toString())
-    const res = await fetch(BASE_URL + `products/${params.category}/?` + parameters.toString(),{cache:'no-store'})
+    const res = await fetch(BASE_URL + `products/${params.category}/?` + parameters.toString(),{cache:'default'})
     if (res.ok) {
         return res.json();
     } else {
@@ -33,6 +32,8 @@ export async function generateMetadata({params, searchParams}) {
     const result = await getData(params, searchParams);
     return metadataGenerator(result?.meta_tag , result?.main_category?.name , `${domainName}/category/${result?.main_category?.url}` , `${domainName}/category/${result?.main_category?.url}` , 'website');
 }
+const ChildCategoryPage = dynamic(()=>import('./childCategoryPage'))
+const ParentCategoryPage = dynamic(()=>import('./parentCategoryPage'))
 
 export default async function Page({params, searchParams}) {
     const data = await getData(params, searchParams);
