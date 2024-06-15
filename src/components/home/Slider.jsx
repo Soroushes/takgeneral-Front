f311e42ce7de530201c38d5f17d95c8d7a6e79ce
hastifadaee <hasti.fadaee2021@gmail.com>
2024-06-15 11:31:35 +0330

    size home

--- src/components/home/Slider.jsx
+++ src/components/home/Slider.jsx
@@ -20,13 +20,13 @@ const Slider = ({slides}) => {
                             return(
                                 <SwiperSlide key={slide.id}>
                                         <Box sx={{aspectRatio: '4.8/1', display:{md:'block' , xs:'none'}}}>
-                                            <Link rel={'preload'} href={slide.link_url ?? '/'}>
-                                                <Image priority loading={'eager'} fill src={slide.image}  alt={slide.alt}/>
+                                            <Link href={slide.link_url ?? '/'}>
+                                                <Image sizes={'small'} priority loading={'eager'} fill src={slide.image}  alt={slide.alt}/>
                                             </Link>
                                         </Box>
                                         <Box sx={{aspectRatio: '1.33/1' , display:{md:'none' , xs:'block'}}}>
-                                            <Link rel={'preload'} href={slide.link_url ?? '/'}>
-                                                <Image priority loading={'eager'} fill src={slide.mobile_image}  alt={slide.alt}/>
+                                            <Link  href={slide.link_url ?? '/'}>
+                                                <Image sizes={'small'} priority loading={'eager'} fill src={slide.mobile_image}  alt={slide.alt}/>
                                             </Link>
                                         </Box>
                                 </SwiperSlide>
