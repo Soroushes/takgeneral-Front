b93ccad8fedb179b56f3072d3d687503f9fe2e2e
hastifadaee <hasti.fadaee2021@gmail.com>
2024-06-16 10:48:41 +0330

    category slider priority

--- src/components/home/HomePageCategorySection.jsx
+++ src/components/home/HomePageCategorySection.jsx
@@ -22,7 +22,7 @@ const HomePageCategorySection = ({mainCategories}) => {
                         <Grid key={item.name} sx={{px: {md: 6, xs: 3}}} xs={6} sm={4} md={3} item>
                             <Link style={{width : '100%'}} href={`/category/${item.url}`}>
                                 <Box sx={{mb: 2 , width : '100%' , textAlign : 'center'}}>
-                                    <Image sizes="(max-width: 232px) 50vw,(max-width: 160px) 33vw" alt={item.name ?? ''} style={{width : '100%' , height : '100%'}} width={160} height={160} src={item.image}/>
+                                    <Image sizes="(max-width: 250px) 50vw,(max-width: 180px) 33vw" alt={item.name ?? ''} style={{width : '100%' , height : '100%'}} width={160} height={160} src={item.image}/>
                                 </Box>
                                 <Typography
                                     variant={'h5'}
